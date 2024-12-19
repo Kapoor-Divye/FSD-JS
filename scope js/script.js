52a49db54f5df31b3b32b048cbbe3237e64a497e
Kapoor-Divye <divyekapoorgzb@gmail.com>
2024-12-19 16:12:24 +0530

    FSD JS updated

--- scope js/script.js
+++ scope js/script.js
@@ -1,5 +1,3 @@
-
-
 // var person={
 //     firstName: "John",
 //     lastName: "Doe",
@@ -153,3 +151,80 @@
 // function sayHello(name,age){
 //     document.write(name+" is "+age+" years old.")
 // }
+
+// function add(a,b){
+//     console.log(a+b)
+//     document.write(a+b)
+// }
+// add(10,20)
+
+// var res=mul(20,30)
+// function mul(x,y){
+//     return x*y
+// }
+// document.write(res)
+
+// const square=function(number){
+//     return number*number
+// }
+// const x=square(4)
+// console.log(x)
+
+// function myFunction(g1,g2){
+//     return g1/g2
+// }
+// const value=myFunction(8,2)
+// console.log(value)
+
+// var add2=(a,b)=>{
+//     console.log(a+b)
+// }
+// add2(30,20)
+
+// var add3=(a,b)=>console.log(a+b)
+// add3(102,20)
+
+// var a=10
+// var b=20
+// var c=30
+// var linebreak="<br/>"
+
+// document.write("a+b+c=")
+// var result=a+b+c
+// document.write(result)
+// document.write(linebreak)
+
+// document.write("a+b=")
+// var result=a+b
+// document.write(result)
+// document.write(linebreak)
+
+// document.write("a/b=")
+// var result=a/b
+// document.write(result)
+// document.write(linebreak)
+
+// function myFunc(){
+//     var x=5+5
+//     console.log(x)
+//     var y="5"+5
+//     console.log(y)
+//     var z="Hello"+5
+//     console.log(z)
+//     var demoP=document.getElementById("demo1")
+//     demoP.innerHTML=x+"<br/>"+y+"<br/>"+z
+// }
+// myFunc()
+
+// if ("5"==5){
+//     console.log("== done")
+// } else{
+//     console.log("== not done")
+// }
+
+// if ("5"===5){
+//     console.log("=== done")
+// } else{
+//     console.log("=== not done")
+// }
+
